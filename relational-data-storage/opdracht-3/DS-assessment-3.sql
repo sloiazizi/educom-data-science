99fa0d33ee845c9f96a08a4a10aa74f9fca4342b
Sarah Loiazizi <sarah.loiazizi@hotmail.com>
2025-09-04 15:53:24 +0200

    Opdracht 3 (#1)

---
+++ relational-data-storage/opdracht-3/DS-assessment-3.sql
@@ -0,0 +1,10 @@
+#oke gebaseerd op tutorial zou ik * kunnen gebruiken
+SELECT *
+FROM track
+LIMIT 20;
+
+#oke mili sec kolom 
+SELECT Milliseconds
+FROM Track
+WHERE Name = 'Princess of the Dawn';
+#antw 375418
